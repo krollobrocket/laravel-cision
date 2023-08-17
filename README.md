207578e7d9e58e572c0d74d2dd681ba02f410927
Krister Andersson <cyclonecode@gmail.com>
2023-08-17 02:10:33 +0200

    Set configuration from dotenv

--- README.md
+++ README.md
@@ -12,6 +12,16 @@ Fetches and exposes news from Cision in Laravel.
     'feed_cache_duration' => 30,
     'feed_base_slug'    => 'The base slug to use for the news feed',
 
+You can also use the following environment variables to set configuration values:
+
+    CISION_FEED_ID=A275C0BF733048FFAE9126ACA64DD08F
+    CISION_FEED_IMAGE_STYLE=UrlTo400x400ArResized
+    CISION_FEED_DATE_FORMAT=Y-m-d
+    CISION_FEED_NUM_ITEMS=50
+    CISION_FEED_ITEMS_PER_PAGE=0
+    CISION_FEED_CACHE_DURATION=0
+    CISION_FEED_BASE_SLUG=ir
+
 ## Installation
 
 First add the repository in your `composer.json` file:
--- config/cision.php
+++ config/cision.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'feed_id'           => 'A275C0BF733048FFAE9126ACA64DD08F',
-    'feed_image_style'  => 'UrlTo400x400ArResized',
-    'feed_date_format'  => 'Y-m-d',
-    'feed_num_items'    => 50,
-    'feed_items_per_page'   => 0,
-    'feed_cache_duration' => 0,
-    'feed_base_slug'    => 'cision',
+    'feed_id'           => env('CISION_FEED_ID', 'A275C0BF733048FFAE9126ACA64DD08F'),
+    'feed_image_style'  => env('CISION_FEED_IMAGE_STYLE', 'UrlTo400x400ArResized'),
+    'feed_date_format'  => env('CISION_FEED_DATE_FORMAT', 'Y-m-d'),
+    'feed_num_items'    => env('CISION_FEED_NUM_ITEMS', 50),
+    'feed_items_per_page'   =>env('CISION_FEED_ITEMS_PER_PAGE', 0),
+    'feed_cache_duration' => env('CISION_FEED_CACHE_DURATION', 0),
+    'feed_base_slug'    => env('CISION_FEED_BASE_SLUG', 'cision'),
 ];
